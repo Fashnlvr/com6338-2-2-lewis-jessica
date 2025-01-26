34ad16c39b7bf38d9d3c9e69792f8b380502397b
p <jlewis0613@outlook.com>
2025-01-25 21:52:57 -0500

    greeting

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code here
+function greet() {
+    const name = prompt("What is your name?");
+    alert(`Hello, ${name}`)
+    const age = parseInt(prompt("How old are you?"), 10);
+    const hasHadBirthday = confirm("Have you had a birthday yet this year? Click 'OK' for Yes or 'Cancel' for No");
+    const currentYear = new Date().getFullYear();
+    const birthYear = hasHadBirthday ? currentYear - age : currentYear - age - 1;
+    alert(`You were born in ${birthYear}.`);
+}
